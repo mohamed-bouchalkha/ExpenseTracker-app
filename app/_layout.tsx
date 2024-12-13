6a7049524d3b48ed171f9368072caf488ef6ab7b
x-don01 <bouchalkha.mohamed@etu.uae.ac.ma>
2024-12-13 16:20:27 +0100

    add home/login/signup pages

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,13 @@
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <Stack>
+      <Stack.Screen name="index" options={{
+        headerTitle:"Home"
+      }} />
+      <Stack.Screen name="LoginScreen" />
+      <Stack.Screen name="SignupScreen" />
+    </Stack>
+  );
 }
